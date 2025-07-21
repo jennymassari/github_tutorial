017b8e769cf7b9e095b2cf0dde66de0fd5b2344f
Jenny Massari <jennymartins13@gmail.com>
2025-07-20 21:14:52 -0500

    added heart

--- main.py
+++ main.py
@@ -14,6 +14,18 @@ snoopy = r"""
     (___| |___)
     """
 print(snoopy)
+
 # No-op change
 
-print("pqp que chato")
+print("My foo coracao for you: \n")
+
+for row in range(6):
+    for col in range(7):
+        if ((row == 0 and col % 3 != 0) or
+            (row == 1 and col % 3 == 0) or
+            (row - col == 2) or
+            (row + col == 8)):
+            print("1", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
